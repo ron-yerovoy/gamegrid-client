e89eac59e0ade856a50f3aef2cfabc1ad7906acb
Adim806 <adim806@gmail.com>
2024-07-19 00:56:49 +0300

    my landing page test_1

--- src/app/components/topBar.js
+++ src/app/components/topBar.js
@@ -1,11 +1,26 @@
 import Link from 'next/link'
 
 export default function Header() {
-    return(
-        <div className="w-screen h-16 bg-transparent p-4 top-0 ">
-           <Link  href='/' className='mx-2 p-1 bg-accent'>Home</Link>
-          
-        </div>
-        
-    );
-};
+  return (
+    <div className="w-screen h-16 bg-transparent p-4 top-0 ">
+      <nav className="text-white p-4 ">
+        <h1
+          className="
+          text-5xl 
+          font-extrabold 
+         text-center 
+          bg-clip-text
+         bg-purple-700 
+          text-transparent 
+         bg-gradient-to-r from-black via-orange-200 to-cyan-500 
+         "
+        >
+          Game-Grid
+        </h1>
+        <Link href="/" className="mx-2 p-1 bg-accent">
+          Home
+        </Link>
+      </nav>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,34 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <Link href="/login">Login</Link>
-      <Link href="/about">about</Link>
+      <div className=" bgrid grid-flow-col grid-rows-2 grid-cols-3 gap-8 bg-red-600">
+        <div>
+          <div className="relative -mt-14 top-[-80px] flex py-0">
+            <Image src="/logo.jpg" width={600} height={400} alt="logo of game grid" />
+          </div>
+        </div>
+        <h1 className="relative py-1 -mt-14 text-4xl items-center justify-center font-semibold">
+          Welcome to the GameGrid!
+          <br />
+          a platform for gamers
+          <br />
+          by gamers
+        </h1>
+      </div>
+      <div class="flex-auto space-x-4 py-40 px-4 bg-blue-500 ">
+        <button class="h-8 px-4 font-semibold bg-green-600 rounded-md text-white" type="submit">
+          Register
+        </button>
+        <button
+          class="h-8 px-4 font-semibold rounded-md border-spacing-6 text-white bg-blue-600"
+          type="button"
+        >
+          Sign in
+        </button>
+      </div>
     </main>
   )
 }
 /* Landing Page */
-
-
