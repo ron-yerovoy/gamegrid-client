4193ac446280d372bf7b1b05e36d1b74aaca8d75
woozai <wooozai@gmail.com>
2024-07-20 21:39:36 +0300

    edited cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3001',
+    screenshotOnRunFailure: false,
+    video: false,
+    retries: 4,
+    defaultCommandTimeout: 15_000,
+    waitForAnimations: true,
+    responseTimeout: 120_000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
