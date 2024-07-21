50786a937f79fbcd987987e31f74d35afba6d945
Homele33 <ontheedge101@gmail.com>
2024-07-21 17:17:18 +0300

    fixed wrong address

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     screenshotOnRunFailure: false,
     video: false,
     retries: 4,
